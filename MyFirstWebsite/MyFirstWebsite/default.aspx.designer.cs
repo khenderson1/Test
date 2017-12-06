25007c9926f62a2bce46b2557a6d490aee0c48c9
Rhodri Mativo <rhodri.mativo@gmail.com>
2017-12-06 13:11:02 -0500

    asdf

--- MyFirstWebsite/MyFirstWebsite/default.aspx.designer.cs
+++ MyFirstWebsite/MyFirstWebsite/default.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace MyFirstWebsite {
         protected global::System.Web.UI.WebControls.Button Button2;
         
         /// <summary>
-        /// Matts_TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Matts_TextBox1;
-        
-        /// <summary>
         /// Image1 control.
         /// </summary>
         /// <remarks>
